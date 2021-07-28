38bcaf5a65ed3341751d693d7da5dfd1461a79d7
Brett Moyo <moyobrett@gmail.com>
2021-07-28 13:00:38 +0200

    added heading

--- src/App.js
+++ src/App.js
@@ -9,29 +9,12 @@ class App extends Component {
     };
     return (
       <div style={widthStyle}>
+        <h1>Minesweeper game</h1>
         <div className="App">
           <br />
           <Map className="map" />
         </div>
-        <p>
-          <a href="https://www.facebook.com/Learn-to-think-like-a-dev-542566672900463">
-            Learn to Think like a Dev
-          </a>
-        </p>
-        <p>How to play minesweeper: (Play on Chrome)</p>
-        <p>1) The goal of the game is to NOT click any bombs</p>
-        <p>
-          2) Each number tells you how many bombs are adjacent to the "clicked"
-          cell.
-        </p>
-        <p>
-          3) The numbers range from 0 to 8. Each cell has 8 neighbors. Count
-          them.
-        </p>
-        <p>4) Use process of elimination to choose where to click next.</p>
-        <br />
-        <br />
-        <br />
+      
       </div>
     );
   }
